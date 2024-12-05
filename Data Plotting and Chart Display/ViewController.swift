ef228a2ccc23bd8c21bbe48735a8d00f1de5a89c
Matthew Dolan <mdolancode@gmail.com>
2024-12-05 10:44:47 -0800

    Initial Commit

---
+++ Data Plotting and Chart Display/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Data Plotting and Chart Display
+//
+//  Created by Matthew Dolan on 2024-12-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
