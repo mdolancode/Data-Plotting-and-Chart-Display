106c5ff2598e927926b35853cde42bf4730dc716
Matthew Dolan <mdolancode@gmail.com>
2024-12-05 12:14:57 -0800

    Instantiate subviews and setup example data for BarChart & LineChart in ViewController

--- Data Plotting and Chart Display/ViewController.swift
+++ Data Plotting and Chart Display/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let barChart = BarChartView()
+    private let lineChart = LineChartView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemIndigo
+        
+        setUpBarChart()
+        setUpLineChart()
     }
-
-
+    
+    func setUpBarChart() {
+        view.addSubview(barChart)
+        // Example Data
+        barChart.data = [10, 20, 15, 30, 25]
+    }
+    
+    func setUpLineChart() {
+        view.addSubview(lineChart)
+        lineChart.data = [10, 20, 15, 30, 25]
+    }
 }
 
